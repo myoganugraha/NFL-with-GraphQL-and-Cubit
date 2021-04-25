68ae84e2c768c81626657275a6897b100b23cc1a
M. Yoga Nugraha <myoganugraha@gmail.com>
2021-04-26 01:05:16 +0700

    change repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:nfl_with_graphql_and_bloc/screen/home_screen.dart';
+import 'package:nfl_with_graphql_and_cubit/screen/home_screen.dart';
 
 void main() {
   runApp(MyApp());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nfl_with_graphql_and_bloc/main.dart';
+import 'package:nfl_with_graphql_and_cubit/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
